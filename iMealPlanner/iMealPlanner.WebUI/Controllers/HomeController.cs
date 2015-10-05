6a78026e6ddf90da3a24bceff24658eb3c2245d8
Chris Beaver <chrisb@pinpointcomms.com.au>
2015-10-05 16:29:19 +1000

    Added Recipe

--- iMealPlanner/iMealPlanner.WebUI/Controllers/HomeController.cs
+++ iMealPlanner/iMealPlanner.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using iMealPlanner.DAL.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,7 @@ namespace iMealPlanner.WebUI.Controllers
     {
         public ActionResult Index()
         {
+            DataContext context = new DataContext();
             return View();
         }
 
