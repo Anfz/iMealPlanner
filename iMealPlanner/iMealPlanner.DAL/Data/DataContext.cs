d4c60063bdc9f13e2de6223bf898b482158cb3a2
Chris Beaver <chrisb@pinpointcomms.com.au>
2015-10-05 16:13:47 +1000

    Commit to I can rollback

---
+++ iMealPlanner/iMealPlanner.DAL/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using iMealPlanner.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iMealPlanner.DAL.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        ///<summary>
+        /// any entity to be persisted must  be declared here
+        /// </summary>
+        
+        public DbSet<Recipe> Recipes { get; set; }
+    }
+}
---
+++ iMealPlanner/iMealPlanner.Model/Recipe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iMealPlanner.Model
+{
+    public class Recipe
+    {
+        public int RecipeId { get; set; }
+        public string Description { get; set; }
+
+        public string RecipeUrl { get; set; }
+
+        public string Instructions { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
